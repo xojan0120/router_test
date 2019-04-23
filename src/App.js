7fce99257c23ef291ce00423e82c9464d8b4c487
xojan0120 <xojan0120@gmail.com>
2019-04-24 08:47:23 +0900

    [Update] ファイル整理

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Home from './Home';
 import Hello from './Hello';
 
-const Home = () => (
-  <div>
-    this is home
-  </div>
-)
 
 class App extends Component {
   render() {
@@ -14,13 +10,25 @@ class App extends Component {
       <Router>
         <ul>
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/hello">Hello</Link></li>
+          <li><Link to="/hello/999">Hello</Link></li>
         </ul>
 
         <hr />
 
-        <Route exact path="/" component={Home} />
-        <Route path="/hello" render={() => <Hello param="hoge" />} />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={Home}
+          />
+
+          <Route 
+            path="/hello/:id"
+            render={(match) => <Hello message="hoge" match={match.match}/>}
+          />
+
+          <Route render={()=><div>not found</div>} />
+        </Switch>
       </Router>
     );
   }
--- src/Hello.js
+++ src/Hello.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-export default props =>  {
+export default props => {
   console.log(props);
   return (
     <div>
       <div>this is hello</div>
-      <div>param is {props.param}</div>
+      <div>message is {props.message}</div>
+      <div>id is {props.match.params.id}</div>
     </div>
   );
 };
